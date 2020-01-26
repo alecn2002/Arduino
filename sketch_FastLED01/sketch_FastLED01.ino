ef6200bfb8cb303a6663e92f04f035382f0c74f4
Unknown <alecn2002@gmail.com>
2020-01-26 12:53:00 +0100

    Touch sensor attached

--- sketch_FastLED01/sketch_FastLED01.ino
+++ sketch_FastLED01/sketch_FastLED01.ino
@@ -1,8 +1,9 @@
 #include "FastLED.h"
 
-#define DELAY 50
+#define DELAY 200
 #define LED_STEP 5
-
+#define LED_PIN 6
+#define BUTTON_PIN 3
 
 template<typename T, int N>
 class RunningLine
@@ -25,6 +26,15 @@ public:
     line[0] = v;
   }
 
+  void pushBack(const T v)
+  {
+    for (size_t i = 0 ; i < N - 1 ; ++i)
+    {
+      line[i] = line[i+1];
+    }
+    line[N-1] = v;
+  }
+
   const T* data() const
   {
     return line;
@@ -45,10 +55,14 @@ class RoundRobin : public RunningLine<T, N>
       }
     }
 
-    T next()
+    T next(bool dir)
     {
-      const T last = RunningLine<T, N>::data()[N-1];
-      RunningLine<T, N>::push(last);
+      const T last = RunningLine<T, N>::data()[dir ? N-1 : 0];
+      if (dir) {
+        RunningLine<T, N>::push(last);
+      } else {
+        RunningLine<T, N>::pushBack(last);
+      }
       return last;
     }
 };
@@ -56,19 +70,26 @@ class RoundRobin : public RunningLine<T, N>
 CRGB init_array[] = { CRGB(128, 0, 0), CRGB(128, 64, 0), CRGB(128, 128, 0), 
                 CRGB(64, 128, 64), CRGB(0, 128,  128), CRGB(0,  64, 128),
                 CRGB(64, 0,  128), CRGB(128, 0, 64), CRGB(128, 0, 0), 
-                CRGB(128, 0, 0)};
+                CRGB(128, 0, 0) };
 
 #define arraySize(a) (sizeof(a) / sizeof(*a))
 #define NUM_LEDS arraySize(init_array)
 
 RoundRobin<CRGB, NUM_LEDS> leds(init_array);
 
+bool fwd = true;
+
 void setup() { 
-    FastLED.addLeds<NEOPIXEL, 6>(const_cast<struct CRGB*>(leds.data()), NUM_LEDS); 
+    FastLED.addLeds<NEOPIXEL, LED_PIN>(const_cast<struct CRGB*>(leds.data()), NUM_LEDS);
+    pinMode(BUTTON_PIN, INPUT);
 }
 
 void loop() {
-  leds.next(); 
+  if (digitalRead(BUTTON_PIN) == HIGH)
+  {
+    fwd = !fwd;
+  }
+  leds.next(fwd); 
   FastLED.show(); 
   delay(DELAY);
 }
