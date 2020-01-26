d2f128db00d3f5e0b14809b4dd11cc0221d77cbe
Unknown <alecn2002@gmail.com>
2020-01-26 16:28:22 +0100

    More palettes

--- sketch_FastLED01/sketch_FastLED01.ino
+++ sketch_FastLED01/sketch_FastLED01.ino
@@ -1,6 +1,6 @@
 #include "FastLED.h"
 
-#define DELAY 200
+#define DELAY 80
 #define LED_STEP 5
 #define LED_PIN 6
 #define BUTTON_PIN 3
@@ -9,7 +9,7 @@ template<typename T, int N>
 class RunningLine
 {
 public:
-  RunningLine(const T init)
+  RunningLine(const T& init)
   {
     for (size_t i = 0 ; i < N ; ++i)
     {
@@ -17,22 +17,22 @@ public:
     }
   }
 
-  void push(const T v)
+  const T& push(const T& v)
   {
     for (size_t i = N - 1 ; i > 0 ; --i)
     {
       line[i] = line[i-1];
     }
-    line[0] = v;
+    return line[0] = v;
   }
 
-  void pushBack(const T v)
+  const T& pushBack(const T& v)
   {
     for (size_t i = 0 ; i < N - 1 ; ++i)
     {
       line[i] = line[i+1];
     }
-    line[N-1] = v;
+    return line[N-1] = v;
   }
 
   const T* data() const
@@ -55,41 +55,92 @@ class RoundRobin : public RunningLine<T, N>
       }
     }
 
-    T next(bool dir)
+    const T& next(bool dir)
     {
       const T last = RunningLine<T, N>::data()[dir ? N-1 : 0];
       if (dir) {
-        RunningLine<T, N>::push(last);
+        return RunningLine<T, N>::push(last);
       } else {
-        RunningLine<T, N>::pushBack(last);
+        return RunningLine<T, N>::pushBack(last);
       }
-      return last;
     }
 };
 
-CRGB init_array[] = { CRGB(128, 0, 0), CRGB(128, 64, 0), CRGB(128, 128, 0), 
+const CRGB init_array[] = { CRGB(128, 0, 0), CRGB(128, 64, 0), CRGB(128, 128, 0), 
                 CRGB(64, 128, 64), CRGB(0, 128,  128), CRGB(0,  64, 128),
                 CRGB(64, 0,  128), CRGB(128, 0, 64), CRGB(128, 0, 0), 
-                CRGB(128, 0, 0) };
+                CRGB(0, 0, 0) };
 
 #define arraySize(a) (sizeof(a) / sizeof(*a))
 #define NUM_LEDS arraySize(init_array)
 
-RoundRobin<CRGB, NUM_LEDS> leds(init_array);
+typedef RoundRobin<CRGB, NUM_LEDS> RR_LED;
+
+RR_LED leds(init_array);
+
+CRGB init_red[NUM_LEDS] = {CRGB(4, 0, 0),  CRGB(16, 0, 0), CRGB(32, 0, 0),
+                           CRGB(64, 0, 0), CRGB(96, 0, 0), CRGB(128, 0, 0),
+                           CRGB(96, 0, 0), CRGB(64, 0, 0), CRGB(32, 0, 0),
+                           CRGB(16, 0, 0)
+};
+
+CRGB init_green[NUM_LEDS] = {CRGB(0, 4, 0),  CRGB(0, 16, 0), CRGB(0, 32, 0),
+                           CRGB(0, 64, 0), CRGB(0, 96, 0), CRGB(0, 128, 0),
+                           CRGB(0, 96, 0), CRGB(0, 64, 0), CRGB(0, 32, 0),
+                           CRGB(0, 16, 0)
+};
+
+CRGB init_blue[NUM_LEDS] = {CRGB(0, 0, 4),  CRGB(0, 0, 16), CRGB(0, 0, 32),
+                           CRGB(0, 0, 64), CRGB(0, 0, 96), CRGB(0, 0, 128),
+                           CRGB(0, 0, 96), CRGB(0, 0, 64), CRGB(0, 0, 32),
+                           CRGB(0, 0, 16)
+};
+
+
+CRGB init_yellow[NUM_LEDS] = {CRGB(4, 4, 0),  CRGB(16, 16, 0), CRGB(32, 32, 0),
+                           CRGB(64, 64, 0), CRGB(96, 96, 0), CRGB(128, 128, 0),
+                           CRGB(96, 96, 0), CRGB(64, 64, 0), CRGB(32, 32, 0),
+                           CRGB(16, 16, 0)
+};
+
+CRGB init_cyan[NUM_LEDS] = {CRGB(0, 4, 4),  CRGB(0, 16, 16), CRGB(0, 32, 32),
+                           CRGB(0, 64, 64), CRGB(0, 96, 96), CRGB(0, 128, 128),
+                           CRGB(0, 96, 96), CRGB(0, 64, 64), CRGB(0, 32, 32),
+                           CRGB(0, 16, 16)
+};
+
+CRGB init_magenta[NUM_LEDS] = {CRGB(4, 0, 4),  CRGB(16, 0, 16), CRGB(32, 0, 32),
+                           CRGB(64, 0, 64), CRGB(96, 0, 96), CRGB(128, 0, 128),
+                           CRGB(96, 0, 96), CRGB(64, 0, 64), CRGB(32, 0, 32),
+                           CRGB(16, 0, 16)
+};
+
+
+const RR_LED leds_rr_init[] = {leds, 
+  RR_LED(init_red), RR_LED(init_green), RR_LED(init_blue),
+  RR_LED(init_yellow), RR_LED(init_cyan), RR_LED(init_magenta),
+};
+
+//RoundRobin<RR_LED, arraySize(leds_rr_init)> leds_rr(leds_rr_init);
 
 bool fwd = true;
+size_t cur_arr = 0;
 
 void setup() { 
-    FastLED.addLeds<NEOPIXEL, LED_PIN>(const_cast<struct CRGB*>(leds.data()), NUM_LEDS);
+    FastLED.addLeds<NEOPIXEL, LED_PIN>(const_cast<struct CRGB*>(leds_rr_init[cur_arr].data()), NUM_LEDS);
     pinMode(BUTTON_PIN, INPUT);
 }
 
 void loop() {
   if (digitalRead(BUTTON_PIN) == HIGH)
   {
+    if(!fwd) {
+      cur_arr = (cur_arr + 1) % arraySize(leds_rr_init);
+      FastLED.addLeds<NEOPIXEL, LED_PIN>(const_cast<struct CRGB*>(leds_rr_init[cur_arr].data()), NUM_LEDS);
+    }
     fwd = !fwd;
   }
-  leds.next(fwd); 
+  leds_rr_init[cur_arr].next(fwd); 
   FastLED.show(); 
   delay(DELAY);
 }
